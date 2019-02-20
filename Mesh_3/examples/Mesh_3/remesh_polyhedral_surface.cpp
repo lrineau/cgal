6237d5f3b42b0bbe1a3a18004d521aab785e157b
Laurent Rineau <laurent.rineau@cgal.org>
2019-02-20 12:09:02 +0100

    Remeshing an OBJ without features

--- Mesh_3/examples/Mesh_3/remesh_polyhedral_surface.cpp
+++ Mesh_3/examples/Mesh_3/remesh_polyhedral_surface.cpp
@@ -1,23 +1,30 @@
+#define CGAL_MESH_3_VERBOSE 1
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
+#include <CGAL/IO/OBJ_reader.h>
 #include <CGAL/Mesh_triangulation_3.h>
 #include <CGAL/Mesh_complex_3_in_triangulation_3.h>
 #include <CGAL/Mesh_criteria_3.h>
+#include <CGAL/Surface_mesh.h>
 
-#include <CGAL/Polyhedral_mesh_domain_with_features_3.h>
+#include <CGAL/Polyhedral_mesh_domain_3.h>
 #include <CGAL/make_mesh_3.h>
+#include <CGAL/IO/facets_in_complex_3_to_triangle_mesh.h>
 
 // Domain 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef CGAL::Polyhedral_mesh_domain_with_features_3<K> Mesh_domain;
+typedef CGAL::Surface_mesh<K::Point_3> Polyhedron;
+typedef CGAL::Polyhedral_mesh_domain_3<Polyhedron, K> Mesh_domain;
 
-// Polyhedron type
-typedef CGAL::Mesh_polyhedron_3<K>::type Polyhedron;
+#if CGAL_CONCURRENT_MESH_3
+typedef CGAL::Parallel_tag Concurrency_tag;
+#else
+typedef CGAL::Sequential_tag Concurrency_tag;
+#endif
 
 // Triangulation
-typedef CGAL::Mesh_triangulation_3<Mesh_domain>::type Tr;
-typedef CGAL::Mesh_complex_3_in_triangulation_3<
-  Tr,Mesh_domain::Corner_index,Mesh_domain::Curve_index> C3t3;
+typedef CGAL::Mesh_triangulation_3<Mesh_domain,CGAL::Default,Concurrency_tag>::type Tr;
+typedef CGAL::Mesh_complex_3_in_triangulation_3<Tr> C3t3;
 
 // Criteria
 typedef CGAL::Mesh_criteria_3<Tr> Mesh_criteria;
@@ -25,13 +32,20 @@ typedef CGAL::Mesh_criteria_3<Tr> Mesh_criteria;
 // To avoid verbose function and named parameters call
 using namespace CGAL::parameters;
 
-int main()
+int main(int argc, char** argv)
 {
-  // Load a polyhedron
-  Polyhedron poly;
-  std::ifstream input("data/lion-head.off");
-  input >> poly;
+  // Load OBJ
+  std::vector<K::Point_3> points;
+  std::vector<std::vector<std::size_t> > faces;
 
+  std::ifstream in(argv[1]);
+  if(!in || !CGAL::read_OBJ(in,points,faces))
+    return 1;
+
+  Polyhedron poly;
+  namespace PMP = CGAL::Polygon_mesh_processing;
+  PMP::orient_polygon_soup(points,faces);
+  PMP::polygon_soup_to_polygon_mesh(points, faces, poly);
   if (!CGAL::is_triangle_mesh(poly)){
     std::cerr << "Input geometry is not triangulated." << std::endl;
     return EXIT_FAILURE;
@@ -45,22 +59,21 @@ int main()
   // empty.
   Mesh_domain domain(poly_ptrs_vector.begin(), poly_ptrs_vector.end());
   
-  // Get sharp features
-  domain.detect_features(); //includes detection of borders
-
   // Mesh criteria
-  Mesh_criteria criteria(edge_size = 0.025,
-                         facet_angle = 25,
-                         facet_size = 0.1,
-                         facet_distance = 0.001);
+  Mesh_criteria criteria(facet_angle = 25,
+                         facet_distance = 0.05);
   
   // Mesh generation
   C3t3 c3t3 = CGAL::make_mesh_3<C3t3>(domain, criteria, no_perturb(), no_exude());
 
   // Output the facets of the c3t3 to an OFF file. The facets will not be
   // oriented.
+  Polyhedron out_surface_mesh;
+  CGAL::facets_in_complex_3_to_triangle_mesh(c3t3, out_surface_mesh);
+
   std::ofstream off_file("out.off");
-  c3t3.output_boundary_to_off(off_file);
+  off_file.precision(17);
+  off_file << out_surface_mesh;
 
   return off_file.fail() ? EXIT_FAILURE : EXIT_SUCCESS;
 }
